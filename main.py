4d7b4d2884fc85a9311119518a3b9630e113e803
JEETDESAI25 <jeetdesai25@gmail.com>
2023-01-21 19:51:00 -0800

    storing the app data in database

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import selectorlib
 import smtplib, ssl
 import os
 import time
+import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 }
+
+connection = sqlite3.connect("data.db")
 
 
 def scrape(url):
@@ -22,6 +25,7 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
+
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
@@ -33,41 +37,48 @@ def send_email(message):
 
     # Reciever Email ID
     receiver = "hetansheeshah@gmail.com"
-    context =ssl.create_default_context()
+    context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, receiver, message)    
+        server.sendmail(username, receiver, message)
     print("Email was sent!")
-    
+
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
-def read (extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date)
+    )
+    rows = cursor.fetchall()
+    return rows
 
 
 if __name__ == "__main__":
+
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
 
-        content = read(extracted)
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 body = ""
-                body = ("Subject: Event" + "\n" + body + "Hey, new event was found!")
+                body = "Subject: Event" + "\n" + body + "Hey, new event was found!"
                 body = body.encode("utf-8")
                 send_email(message=body)
-    
-        time.sleep(360)    
-            
 
-"INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
-"SELECT * FROM events WHERE date='2088.10.14'"
+        time.sleep(2)
