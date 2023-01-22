5a45c0246360e32f6d14d0d7f76b07a028d45a21
JEETDESAI25 <jeetdesai25@gmail.com>
2023-01-21 20:34:53 -0800

    Changes

--- main.py
+++ main.py
@@ -10,75 +10,80 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 }
 
-connection = sqlite3.connect("data.db")
+
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
+
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
+
+        # Sender's email ID
+        username = ""
+        # Sender's gmail app password
+        password = os.getenv("PASSWORD")
+
+        # Reciever Email ID
+        receiver = ""
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was sent!")
+
+class Database:
+
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
+
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
 
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
-
-
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
-
-
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
-
-    # Sender's email ID
-    username = ""
-    # Sender's gmail app password
-    password = os.getenv("PASSWORD")
-
-    # Reciever Email ID
-    receiver = ""
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
-
-
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
-
-
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date)
-    )
-    rows = cursor.fetchall()
-    return rows
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date)
+        )
+        rows = cursor.fetchall()
+        return rows
 
 
 if __name__ == "__main__":
 
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
+                database.store(extracted)
                 body = ""
                 body = "Subject: Event" + "\n" + body + "Hey, new event was found!"
                 body = body.encode("utf-8")
-                send_email(message=body)
+                email=Email()
+                email.send(message=body)
 
         time.sleep(2)
